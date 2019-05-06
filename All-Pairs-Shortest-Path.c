2f89c744b03a30505d0b0a549d49688cfbdd254a
ArjunAloshious <47184341+ArjunAloshious@users.noreply.github.com>
2019-05-06 10:31:29 +0530

    Add files via upload

---
+++ All-Pairs-Shortest-Path.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+#define V 4
+#define INF 9999
+
+void disp_mat(int mat[][V])
+{
+  for(int i=0;i<V;++i)
+    {
+      for(int j=0;j<V;++j)
+				mat[i][j]==INF?printf("%-7s","INF"):printf("%-7d",mat[i][j]);
+      printf("\n");
+    }
+}  
+
+int main()
+{
+  int mat[V][V]={{0,5,INF,10}, 
+                 {INF,0,3,INF}, 
+								 {INF,INF,0,1}, 
+								 {INF,INF,INF,0}};
+  printf("Adjacency matrix : \n");
+  disp_mat(mat);
+  for(int i=0;i<V;++i)
+  {
+    for(int j=0;j<V;++j)
+		{
+			for(int k=0;k<V;++k)
+			{
+		 		if(mat[j][k]>mat[j][i]+mat[i][k])
+				{
+					mat[j][k]=mat[j][i]+mat[i][k];
+				}
+			}
+		}
+  }
+  printf("\nShortest paths : \n");
+  disp_mat(mat);
+  return 0;
+}
